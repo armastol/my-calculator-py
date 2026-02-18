b68f6a52555cc302300117fd706161589b98bdaa
armastol <giovannitoledo.dev@gmail.com>
2026-02-18 20:11:04 +0000

    Round addition to two digits and fixed division by zero.

--- calculator.py
+++ calculator.py
@@ -1,25 +1,33 @@
-a = float(input("Write the first number:"))
-b = float(input("Write the second number:"))
-print("Choose an option:")
-print("1/Sum.")
-print("2/Sumtract.")
-print("3/Multiplication")
-print("4/Division")
+running = True
+while running:
+  a = float(input("Write the first number:"))
+  b = float(input("Write the second number:"))
+  print("Choose an option:")
+  print("1/Sum.")
+  print("2/Sumtract.")
+  print("3/Multiplication")
+  print("4/Division")
+  option = int(input("Enter your number choice:"))
 
-option = int(input("Enter 1 or 2:"))
+  if option == 1: 
+   result = round(a + b, 2)
+   print(f"The result is {result:g}.")
 
-if option == 1: 
-   result = a + b
-   print(f"The result is {result}.")
+  elif option == 2: 
+   result = round(a - b, 2)
+   print(f"The result is {result:g}.")
 
-elif option == 2: 
-   result = a - b
-   print(f"The result is {result}.")
+  elif option == 3:
+   result = round(a * b, 2)
+   print(f"The result is {result:g}.")
 
-elif option == 3:
-   result = a*b 
-   print(f"The result is {result}.")
+  elif option == 4:
+   if b == 0:
+    print("You cannot divide by zero.")
+   elif b != 0:
+    result = round(a / b, 2)
+    print(f"The result is {result:g}.")
 
-elif option == 4:
-   result = a / b
-   print(f"The result is {result}.")
+  elif option == 5:
+   print("Goodbay")
+   running = False 

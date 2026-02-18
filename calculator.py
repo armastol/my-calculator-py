248b9c54aa725a4e68a3847641f65a91aa907bb0
armastol <giovannitoledo.dev@gmail.com>
2026-02-18 19:11:35 +0000

    Fix: clean code and no conflict flags

--- calculator.py
+++ calculator.py
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
 a = float(input("Write the first number:"))
 b = float(input("Write the second number:"))
-
-print("Choose an option:")
-print("1/Sum.")
-print("2/Sumtract.")
-
-option = int(input("Enter 1 or 2:"))
-
-if option == 1: 
-   result = a + b
-   print(f"The result is {result}.")
-
-elif option == 2: 
-   result = a - b
-=======
-a = float(input("Write the first number:"))
-b = float(input("Write the second number:"))
-
 print("Choose an option:")
 print("1/Sum.")
 print("2/Sumtract.")
@@ -40,5 +22,4 @@ elif option == 3:
 
 elif option == 4:
    result = a / b
->>>>>>> 45ca2ff (I added multiplication and division)
    print(f"The result is {result}.")
